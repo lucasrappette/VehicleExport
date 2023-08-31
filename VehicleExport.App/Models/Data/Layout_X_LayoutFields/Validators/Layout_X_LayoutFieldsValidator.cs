b83f3ce6d441a62723a2104bdb290eac381e242e
geggold <140199015+geggold@users.noreply.github.com>
2023-08-31 14:26:53 -0500

    Remove Layout_x_LayoutFields

--- VehicleExport.App/Models/Data/Layout_X_LayoutFields/Validators/Layout_X_LayoutFieldsValidator.cs
+++
@@ -1,8 +0,0 @@
-﻿using FluentValidation;
-
-namespace VehicleExport.App.Models.Data.Layout_X_LayoutFields.Validators
-{
-    public class Layout_X_LayoutFieldsValidator : AbstractValidator<Layout_X_LayoutField>
-    {
-    }
-}
